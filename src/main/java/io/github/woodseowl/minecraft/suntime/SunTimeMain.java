5ffe12fff10ff20b75eda8df87c61a6c79a6fd0d
Eric Woods <woodseowl@gmail.com>
2015-04-05 19:48:57 -0400

    Initial SunTime configuration

---
+++ src/main/java/io/github/woodseowl/minecraft/suntime/SunTimeMain.java
@@ -0,0 +1,10 @@
+package io.github.woodseowl.minecraft.suntime;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SunTimeMain extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("SunTime does nothing right now");
+    }
+}
---
+++ src/test/java/io/github/woodseowl/minecraft/suntime/SunTimeMainTest.java
@@ -0,0 +1,7 @@
+package io.github.woodseowl.minecraft.suntime;
+
+import static org.junit.Assert.*;
+
+public class SunTimeMainTest {
+
+}
