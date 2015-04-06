42e3537cc25cfb5ba46e49637f1aeab3fb5e48e2
Eric Woods <woodseowl@gmail.com>
2015-04-06 01:59:48 -0400

    Basic time reporting and conversion started

--- src/main/java/io/github/woodseowl/minecraft/suntime/Main.java
+++ src/main/java/io/github/woodseowl/minecraft/suntime/Main.java
@@ -1,11 +1,27 @@
 package io.github.woodseowl.minecraft.suntime;
 
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
-        TimeHelpers time = new TimeHelpers();
+        World world = getServer().getWorlds().get(0);
+        TimeHelpers time = new TimeHelpers(world);
+
+        world.setGameRuleValue("doDaylightCycle", "false");
+
         getLogger().info("SunTime thinks it is " + time.getRealTimeString());
+
+        getLogger().info("SunTime thinks the real time is " + time.getRealTimeLong());
+
+        getLogger().info("SunTime thinks the server time is " + time.getWorldTime());
+
+        getLogger().info("SunTime thinks the server full time is " + time.getWorldFullTime());
+
+        getLogger().info("SunTime wants to set the server time to " + time.convertRealTimeToWorldTime(time.getRealTimeLong()));
+
+        getLogger().info("DaylightCycle status is " + world.getGameRuleValue("doDaylightCycle"));
+
     }
 }
--- src/main/java/io/github/woodseowl/minecraft/suntime/TimeHelpers.java
+++ src/main/java/io/github/woodseowl/minecraft/suntime/TimeHelpers.java
@@ -1,25 +1,62 @@
 package io.github.woodseowl.minecraft.suntime;
 
+import org.bukkit.World;
+
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Calendar;
 
 public class TimeHelpers {
-    Date realTime;
+    public static final int DAYLENGTH = 24000;
+    public static final int SUNRISE = 0;
+    public static final int SUNSET = 12000;
 
-    public TimeHelpers() {
-        realTime = new Date();
+    Calendar realDate;
+    private World world;
+
+    public TimeHelpers(World world) {
+        this.world = world;
+        realDate = Calendar.getInstance();
     }
 
-    public TimeHelpers(Date realTime) {
-        this.realTime = realTime;
-    }
-
-    public final Date getRealTime() {
-        return realTime;
+    public TimeHelpers(World world, Calendar realDate) {
+        this.world = world;
+        this.realDate = realDate;
     }
 
     public final String getRealTimeString() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm aaa z");
-        return dateFormat.format(realTime);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm z");
+        return dateFormat.format(realDate.getTime());
+    }
+
+    public final Long getRealTimeLong() {
+        int hour = realDate.get(Calendar.HOUR_OF_DAY) * 1000;
+        Double minute = realDate.get(Calendar.MINUTE) * 1000 / 60.0;
+        return Math.round(hour + minute);
+    }
+
+    public final Long convertRealTimeToWorldTime(Long realTime) {
+        double realSunrise = 6000;
+        double realSunset = 18000;
+
+        if (realTime > realSunrise && realTime < realSunset) {
+            double dayFraction = (realTime - realSunrise) / (realSunset - realSunrise);
+            return Math.round((SUNSET - SUNRISE) * dayFraction) + SUNRISE;
+        } else {
+            double nightFraction;
+            if (realTime <= realSunrise) {
+                nightFraction = (DAYLENGTH + realTime - realSunset) / (DAYLENGTH + realSunrise - realSunset);
+            } else {
+                nightFraction = (realTime - realSunset) / (DAYLENGTH + realSunrise - realSunset);
+            }
+            return Math.round((DAYLENGTH - SUNSET) * nightFraction) + SUNSET;
+        }
+    }
+
+    public final Double getWorldTime() {
+        return world.getTime() / 1000.0;
+    }
+
+    public final Double getWorldFullTime() {
+        return world.getFullTime() / 1000.0;
     }
 }
