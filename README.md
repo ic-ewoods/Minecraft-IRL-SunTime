31b2e231c88be2cda7f7a08ce186b85247739ed0
Eric Woods <woodseowl@gmail.com>
2015-04-07 19:07:09 -0400

    Cleanup, todo

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# minecraft-suntime
+# SunTime
 A Bukkit plug-in to have Minecraft time track to real world time
+
+---
+
+ToDo
+---
+
+**Simple Commands**
+ 
+ * realtime: print realtime to your console)
+ * sunrise|sunset: print time since / until sunrise|sunset
+ 
+**Configuration**
+
+ - timezone
+ - lat / lon
+ 
+**Weather**
+
+ - get realweather (WeatherUnderground API)
+ 
--- src/main/java/io/github/woodseowl/minecraft/suntime/SunCalculator.java
+++ src/main/java/io/github/woodseowl/minecraft/suntime/SunCalculator.java
@@ -1,32 +1,24 @@
 package io.github.woodseowl.minecraft.suntime;
 
-//import com.luckycatlabs.sunrisesunset.SunriseSunsetCalculator;
-//import com.luckycatlabs.sunrisesunset.dto.Location;
+import com.luckycatlabs.sunrisesunset.SunriseSunsetCalculator;
+import com.luckycatlabs.sunrisesunset.dto.Location;
 
 import java.util.Calendar;
 
 public class SunCalculator {
 
-//    private SunriseSunsetCalculator calculator;
+    private SunriseSunsetCalculator calculator;
 
     public SunCalculator() {
-//        Location location = new Location("42", "-76");
-//        calculator = new SunriseSunsetCalculator(location, "America/New_York");
+        Location location = new Location("42", "-76");
+        calculator = new SunriseSunsetCalculator(location, "America/New_York");
     }
 
     public Calendar getSunrise() {
-        Calendar sunrise = Calendar.getInstance();
-        sunrise.set(Calendar.HOUR, 5);
-        sunrise.set(Calendar.MINUTE, 30);
-        return sunrise;
-        // return calculator.getOfficialSunriseCalendarForDate(Calendar.getInstance());
+        return calculator.getOfficialSunriseCalendarForDate(Calendar.getInstance());
     }
 
     public Calendar getSunset() {
-        Calendar sunset = Calendar.getInstance();
-        sunset.set(Calendar.HOUR, 18);
-        sunset.set(Calendar.MINUTE, 30);
-        return sunset;
-        // return calculator.getOfficialSunsetCalendarForDate(Calendar.getInstance());
+        return calculator.getOfficialSunsetCalendarForDate(Calendar.getInstance());
     }
 }
